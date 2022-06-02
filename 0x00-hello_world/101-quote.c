e345a560ab10e88458e8561d6a719544d4c527fc
JohnObiero <owuorj633@gmail.com>
2022-06-02 14:18:10 -0700

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,10 @@
 #include <unistd.h>
 
 /**
- *
- *  * main - my entry point
- *
- *   * Description: 'and that piece of art is useful" - Dora korpar, 2015-10-19'
- *
- *    * Return: 1
- *
- *     */
+ * main - my entry point
+ * Description: 'and that piece of art is useful" - Dora korpar, 2015-10-19'
+ * Return: 1
+ */
 
 int main(void)
 
