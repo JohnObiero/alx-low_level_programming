fef3eaa88cdd0b95600b93475a2e1aace00d9c7d
JohnObiero <owuorj633@gmail.com>
2022-06-07 16:28:26 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,10 +9,9 @@
 void jack_bauer(void)
 {
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+
 	hours_max = 58;
-
 	hours_tens = '0';
-
 	while (hours_tens < '3')
 	{
 		if (hours_tens == '2')
@@ -45,5 +44,4 @@ void jack_bauer(void)
 		}
 		hours_ones = '0';
 		hours_tens++;
-}
 }
