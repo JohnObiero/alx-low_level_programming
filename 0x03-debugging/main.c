71f359dd4180af87cd2d0dc599d6292952eb7e5f
JohnObiero <owuorj633@gmail.com>
2022-06-09 15:36:37 -0700

    task 0

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -7,32 +7,20 @@
 #include <stdio.h>
 
 /* betty style doc for function main goes there */
-
 /**
-
  * main - used to check the condition
-
  *
-
  * Description: return the result of the function
-
  *
-
  * Return: 0
-
  *
-
  */
-
-
 
 int main(void)
 
 {
 
 	int n;
-
-
 
 	srand(time(0));
 
@@ -49,19 +37,13 @@ int main(void)
 	} else if (n == 0)
 
 	{
-
 		printf("%d is zero\n", n);
 
 	} else
 
 	{
-
 		printf("%d is negative\n", n);
-
 	}
 
-
-
 	return (0);
-
 }
