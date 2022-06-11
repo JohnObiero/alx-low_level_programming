41ced67b89d3050022651b130e747bc6a36ccc65
JohnObiero <owuorj633@gmail.com>
2022-06-11 11:42:58 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,15 +3,10 @@
 
 
 /**
-
  * print_most_numbers - a function that prints the numbers, from 0 to 9.
-
  * Do not print 2 and 4.
-
  * followed by a new line.
-
  * Return: Always 0
-
  */
 
 void print_most_numbers(void)
@@ -33,5 +28,4 @@ void print_most_numbers(void)
 	}
 
 	_putchar('\n');
-
 }
