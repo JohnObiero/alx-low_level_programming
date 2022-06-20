63ec55b7b3ba05889c88fb6a86357c9e20c602d7
JohnObiero <owuorj633@gmail.com>
2022-06-20 12:54:46 -0700

    Advance task

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,15 +3,10 @@
 
 
 /**
-
  * set_string - sets the value of a pointer to a char
-
  * @s: source adress.
-
  * @to: target adress.
-
  * Return: no return.
-
  */
 
 void set_string(char **s, char *to)
@@ -19,5 +14,4 @@ void set_string(char **s, char *to)
 {
 
 	*s = to;
-
 }
