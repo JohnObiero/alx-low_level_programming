06a47d49cc43f18e73cae2bffe7f68a91adb7af7
JohnObiero <owuorj633@gmail.com>
2022-06-26 13:18:59 -0700

    Static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+
+/**
+ * _isupper - writes the character c to stdout
+ * @c: The character check
+ * Return: 1 if upper 0  if lower.
+ */
+
+int _isupper(int c)
+
+{
+
+	if (c < 65 || c > 90)
+
+	{
+
+		return (0);
+
+	}
+
+	else
+
+	{
+
+		return (1);
+
+	}
+
+}
