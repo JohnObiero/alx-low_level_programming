0271cfaed613f12d095ca84a8bc04dac3fdc1d70
JohnObiero <owuorj633@gmail.com>
2022-06-26 13:57:18 -0700

    Static Libraries

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+
+ * _strlen - returns the length of string
+
+ * @s: string to return length
+
+ * Return: length of @s
+
+ */
+
+int _strlen(char *s)
+
+{
+
+	int length = 0;
+
+
+
+	while (*s++)
+
+		length++;
+
+	return (length);
+
+}
+
+
+
+/**
+
+ * _strcat - appends a string to another
+
+ * @src: string to append
+
+ * @dest: string to append to
+
+ * Return: appended string
+
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+
+	int dest_len = _strlen(dest);
+
+	int i;
+
+
+
+	for (i = 0; src[i] != '\0'; i++)
+
+		dest[dest_len + i] = src[i];
+
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+
+}
